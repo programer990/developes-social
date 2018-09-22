23667186b24138cd27e3e69a47c57c0a56856a4e
khadijah hamed <khadijah@khadijahs-MacBook-Pro.local>
2018-09-21 19:28:39 -0700

    Sign in ui custom classes complete

---
+++ developes-social/Util/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  developes-social
+//
+//  Created by khadijah hamed on 9/21/18.
+//  Copyright © 2018 khadijah hamed. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ developes-social/View/FancyField.swift
@@ -0,0 +1,29 @@
+//
+//  FancyField.swift
+//  developes-social
+//
+//  Created by khadijah hamed on 9/21/18.
+//  Copyright © 2018 khadijah hamed. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+
+}
